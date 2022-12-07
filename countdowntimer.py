250edf4b23a67a541929bc0170d58bb26c65cbc7
Pal <deepankar.deepankar@accenture.com>
2022-12-07 20:39:46 +0530

    fresh start up

--- countdowntimer.py
+++
@@ -1,9 +0,0 @@
-import time
-userinput = int(input("Enter number of seconds: "))
-
-while(userinput):
-    mins, secs = divmod(userinput, 60)
-    timer = f"{mins:02d}:{secs:02d}"
-    print(timer,end='\r')
-    time.sleep(1)
-    userinput-=1
--- pattern.py
+++
@@ -1,4 +0,0 @@
-number = int(input("Enter a number between 0-9: "))
-
-for i in range(1,number+1):
-    print("*"*i)
